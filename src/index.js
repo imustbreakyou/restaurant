0e39d1d7e44c014f2cdf2029059c9240ccf4801d
Mike <mw@Mikes-MacBook-Pro.local>
2024-02-23 15:12:23 -0800

    implement background image

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 import './style.css';
+import Icon from './icon.png'
 console.log("index.js served!")
+
+  // Add the image to our existing div.
+  const myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
+
